1c1d12752fca4c8f88893271d92b47681a6ee557
JasonYao <31233752+JasonYao81000@users.noreply.github.com>
2019-05-18 21:49:02 +0800

    Delete mario.bat

--- hw3/mario.bat
+++
@@ -1,4 +0,0 @@
-python main.py --train_mario --world 1 --stage 1
-python main.py --train_mario --world 1 --stage 2 --restore a2c-1-1.cpt
-python main.py --train_mario --world 1 --stage 3 --restore a2c-1-2.cpt
-python main.py --train_mario --world 1 --stage 4 --restore a2c-1-3.cpt
