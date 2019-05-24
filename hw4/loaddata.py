9c5384e95675f987e4f02f6bcb2674942a6d34d3
wangssuming <r07945029@ntu.edu.tw>
2019-05-24 15:12:04 +0800

    experiment2 tanh>sig

--- hw4/loaddata.py
+++ hw4/loaddata.py
@@ -12,35 +12,50 @@ from PIL import Image
 
 class Dataset(data.Dataset):
     'Characterizes a dataset for PyTorch'
-    def __init__(self, root):
+    def __init__(self, test, root):
         'Initialization'
         list_IDs = []
         labels = []
+        self.test = test
         self.root = root
         self.transform = transforms.Compose([transforms.RandomHorizontalFlip(p=0.5),
-                                             transforms.ToTensor(),
-                                             transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
-        with open(self.root + 'cartoon_attr.txt', encoding="utf-8") as f:
-            lines = f.readlines()
-        lines = lines[2:]
-        for line in lines:
-            list_IDs.append(line.split('png')[0]+'png')
-            labels.append(line.rstrip('\n').split('png ')[1].split(' '))
-        self.labels = np.array(labels).astype(int)
-        self.list_IDs = list_IDs
+                                             transforms.ToTensor()])
+        if self.test:
+            with open(self.root + 'sample_human_testing_labels.txt', encoding="utf-8") as f:
+                lines = f.readlines()
+            lines = lines[2:]
+            for line in lines:
+                labels.append(line.rstrip('\n').split(' '))
+            self.labels = np.array(labels).astype(int)
+        else:
+            with open(self.root + 'cartoon_attr.txt', encoding="utf-8") as f:
+                lines = f.readlines()
+            lines = lines[2:]
+            for line in lines:
+                list_IDs.append(line.split('png')[0]+'png')
+                labels.append(line.rstrip('\n').split('png ')[1].split(' '))
+            self.labels = np.array(labels).astype(int)
+            self.list_IDs = list_IDs
 
     def __len__(self):
         'Denotes the total number of samples'
-        return len(self.list_IDs)
+        if self.test:
+            return len(self.labels)
+        else:
+            return len(self.list_IDs)
 
     def __getitem__(self, index):
         'Generates one sample of data'
-        # Select sample
-        ID = self.list_IDs[index]
-
-        # Load data and get label
-        img = Image.open(self.root + 'images/' + ID)
-        img = self.transform(img)
-        label = self.labels[index]
-
-        return img, label
+        if self.test:
+            label = self.labels[index]
+            return label
+        else:
+            # Select sample
+            ID = self.list_IDs[index]
+    
+            # Load data and get label
+            img = Image.open(self.root + 'images/' + ID)
+            img = self.transform(img)
+            label = self.labels[index]
+    
+            return img, label
