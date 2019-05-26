791d165447e818ed47b74de2e83a8a28145f6324
JasonYao <31233752+JasonYao81000@users.noreply.github.com>
2019-05-26 18:25:12 +0800

    Build Resnet-base generator and the default architecture is `Resnet-18`.

--- hw4/acgan_resnet.py
+++ hw4/acgan_resnet.py
@@ -189,6 +189,174 @@ def _resnet(arch, block, layers, **kwargs):
     model = ResNet(block, layers, **kwargs)
     return model
 
+class BasicBlock_Generator(nn.Module):
+    expansion = 1
+
+    def __init__(self, inplanes, planes, stride=1, upsample=None, groups=1,
+                 base_width=64, dilation=1, norm_layer=None):
+        super(BasicBlock_Generator, self).__init__()
+        if norm_layer is None:
+            norm_layer = nn.BatchNorm2d
+        if groups != 1 or base_width != 64:
+            raise ValueError('BasicBlock only supports groups=1 and base_width=64')
+        if dilation > 1:
+            raise NotImplementedError("Dilation > 1 not supported in BasicBlock")
+        # Both self.conv1_upsample and self.upsample layers upsample the input when stride != 1
+        self.conv1 = conv3x3(inplanes, planes)
+        self.conv1_upsample = nn.Upsample(scale_factor=stride)
+        self.bn1 = norm_layer(planes)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = conv3x3(planes, planes)
+        self.bn2 = norm_layer(planes)
+        self.stride = stride
+        self.upsample = upsample
+
+    def forward(self, x):
+        identity = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+        
+        if self.stride != 1 :
+            out = self.conv1_upsample(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+
+        if self.upsample is not None:
+            identity = self.upsample(x)
+        
+        out += identity
+        out = self.relu(out)
+        
+        return out
+
+class Bottleneck_Generator(nn.Module):
+    expansion = 4
+
+    def __init__(self, inplanes, planes, stride=1, upsample=None, groups=1,
+                 base_width=64, dilation=1, norm_layer=None):
+        super(Bottleneck_Generator, self).__init__()
+        if norm_layer is None:
+            norm_layer = nn.BatchNorm2d
+        width = int(planes * (base_width / 64.)) * groups
+        # Both self.conv2_upsample and self.upsample layers upsample the input when stride != 1
+        self.conv1 = conv1x1(inplanes, width)
+        self.bn1 = norm_layer(width)
+        self.conv2 = conv3x3(width, width, groups, dilation)
+        self.conv2_upsample = nn.Upsample(scale_factor=stride)
+        self.bn2 = norm_layer(width)
+        self.conv3 = conv1x1(width, planes * self.expansion)
+        self.bn3 = norm_layer(planes * self.expansion)
+        self.relu = nn.ReLU(inplace=True)
+        self.upsample = upsample
+        self.stride = stride
+
+    def forward(self, x):
+        identity = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+        out = self.relu(out)
+        
+        if self.stride != 1 :
+            out = self.conv2_upsample(out)
+
+        out = self.conv3(out)
+        out = self.bn3(out)
+
+        if self.upsample is not None:
+            identity = self.upsample(x)
+
+        out += identity
+        out = self.relu(out)
+
+        return out
+
+class ResNet_Generator(nn.Module):
+    def __init__(self, block, layers, zero_init_residual=False,
+                 groups=1, width_per_group=64,
+                 norm_layer=None):
+        super(ResNet_Generator, self).__init__()
+        if norm_layer is None:
+            norm_layer = nn.BatchNorm2d
+        self._norm_layer = norm_layer
+
+        self.inplanes = 64
+        self.dilation = 1
+        
+        self.groups = groups
+        self.base_width = width_per_group
+        self.layer1 = self._make_layer(block, 64, layers[0])
+        self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
+        self.layer3 = self._make_layer(block, 256, layers[2], stride=2)
+        self.layer4 = self._make_layer(block, 512, layers[3], stride=2)
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+            elif isinstance(m, (nn.BatchNorm2d, nn.GroupNorm)):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+
+        # Zero-initialize the last BN in each residual branch,
+        # so that the residual branch starts with zeros, and each residual block behaves like an identity.
+        # This improves the model by 0.2~0.3% according to https://arxiv.org/abs/1706.02677
+        if zero_init_residual:
+            for m in self.modules():
+                if isinstance(m, Bottleneck):
+                    nn.init.constant_(m.bn3.weight, 0)
+                elif isinstance(m, BasicBlock):
+                    nn.init.constant_(m.bn2.weight, 0)
+
+    def _make_layer(self, block, planes, blocks, stride=1, dilate=False):
+        norm_layer = self._norm_layer
+        upsample = None
+        previous_dilation = self.dilation
+        if dilate:
+            self.dilation *= stride
+            stride = 1
+        if stride != 1 or self.inplanes != planes * block.expansion:
+            if stride == 1:
+                upsample = nn.Sequential(
+                    conv1x1(self.inplanes, planes * block.expansion, stride),
+                    norm_layer(planes * block.expansion),
+                )
+            else:
+                upsample = nn.Sequential(
+                    nn.Upsample(scale_factor=stride),
+                    conv1x1(self.inplanes, planes * block.expansion, 1),
+                    norm_layer(planes * block.expansion),
+                )
+
+        layers = []
+        layers.append(block(self.inplanes, planes, stride, upsample, self.groups,
+                            self.base_width, previous_dilation, norm_layer))
+        self.inplanes = planes * block.expansion
+        for _ in range(1, blocks):
+            layers.append(block(self.inplanes, planes, groups=self.groups,
+                                base_width=self.base_width, dilation=self.dilation,
+                                norm_layer=norm_layer))
+
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+
+        return x
+
+def _resnet_Generator(arch, block, layers, **kwargs):
+    model = ResNet_Generator(block, layers, **kwargs)
+    return model
+
 class Generator(nn.Module):
     def __init__(self, z_dim, datasets):
         super(Generator, self).__init__()
@@ -199,20 +367,14 @@ class Generator(nn.Module):
         self.glasses_encoded = torch.eye(len(datasets.attr_glasses)).cuda()
         self.hidden_emb = nn.Linear(z_dim, z_dim, bias=False)
 
-        self.init_size = IMAGE_SIZE // 4  # Initial size before upsampling
-        self.l1 = nn.Sequential(nn.Linear(z_dim + datasets.labels.shape[1], 128 * self.init_size ** 2))
+        self.init_size = IMAGE_SIZE // 8  # Initial size before upsampling
+        self.l1 = nn.Sequential(nn.Linear(z_dim + datasets.labels.shape[1], 64 * self.init_size ** 2))
+
+        self.resnet = _resnet_Generator('resnet18', BasicBlock_Generator, [2, 2, 2, 2])
 
         self.conv_blocks = nn.Sequential(
-            nn.BatchNorm2d(128),
-            nn.Upsample(scale_factor=2),
-            nn.Conv2d(128, 128, 3, stride=1, padding=1),
-            nn.BatchNorm2d(128, 0.8),
-            nn.LeakyReLU(0.2, inplace=True),
-            nn.Upsample(scale_factor=2),
-            nn.Conv2d(128, 64, 3, stride=1, padding=1),
-            nn.BatchNorm2d(64, 0.8),
-            nn.LeakyReLU(0.2, inplace=True),
-            nn.Conv2d(64, IMAGE_CHANNELS, 3, stride=1, padding=1),
+            self.resnet,
+            nn.Conv2d(512, IMAGE_CHANNELS, 3, stride=1, padding=1),
             nn.Tanh(),
         )
 
@@ -224,7 +386,7 @@ class Generator(nn.Module):
             self.glasses_encoded[glasses_idxes].cuda()), dim=-1)
         gen_input = torch.cat((labels, self.hidden_emb(noise)), dim=-1)
         out = self.l1(gen_input)
-        out = out.view(out.shape[0], 128, self.init_size, self.init_size)
+        out = out.view(out.shape[0], 64, self.init_size, self.init_size)
         img = self.conv_blocks(out)
         return img
 
@@ -232,8 +394,8 @@ class Discriminator(nn.Module):
     def __init__(self, datasets):
         super(Discriminator, self).__init__()
         
-        self.resnet_50 = _resnet('resnet50', Bottleneck, [3, 4, 6, 3])
-        flatten_size = 512 * Bottleneck.expansion
+        self.resnet = _resnet('resnet18', BasicBlock, [2, 2, 2, 2])
+        flatten_size = 512 * BasicBlock.expansion
 
         # Output layers
         self.adv_layer = nn.Sequential(nn.Linear(flatten_size, 1), nn.Sigmoid())
@@ -243,7 +405,7 @@ class Discriminator(nn.Module):
         self.aux_layer_glasses = nn.Sequential(nn.Linear(flatten_size, len(datasets.attr_glasses)), nn.Softmax(dim=-1))
 
     def forward(self, img):
-        out = self.resnet_50(img)
+        out = self.resnet(img)
         validity = self.adv_layer(out)
         hair = self.aux_layer_hair(out)
         eye = self.aux_layer_eye(out)
