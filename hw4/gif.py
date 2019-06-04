9e35f4e41ac07db3c50ca168dc0e1549e2ce8572
JasonYao <31233752+JasonYao81000@users.noreply.github.com>
2019-06-04 23:21:21 +0800

    Create training progress GIFs for both architectures.

---
+++ hw4/gif.py
@@ -0,0 +1,30 @@
+from tqdm.auto import tqdm
+import imageio
+
+image_dir = './eval_images/acgan_500/'
+writer = imageio.get_writer(image_dir + 'acgan_500.gif', fps=2)
+for i in tqdm(range(0, 100, 5)):
+    fname = image_dir + '%d.png' % (i)
+    im = imageio.imread(fname)
+    writer.append_data(im)
+for i in tqdm(range(100, 500, 50)):
+    fname = image_dir + '%d.png' % (i)
+    im = imageio.imread(fname)
+    writer.append_data(im)
+writer.close()
+
+image_dir = './eval_images/resnet_1000/'
+writer = imageio.get_writer(image_dir + 'resnet_1000.gif', fps=2)
+for i in tqdm(range(0, 100, 5)):
+    fname = image_dir + '%d.png' % (i)
+    im = imageio.imread(fname)
+    writer.append_data(im)
+for i in tqdm(range(100, 500, 50)):
+    fname = image_dir + '%d.png' % (i)
+    im = imageio.imread(fname)
+    writer.append_data(im)
+for i in tqdm(range(500, 1000, 100)):
+    fname = image_dir + '%d.png' % (i)
+    im = imageio.imread(fname)
+    writer.append_data(im)
+writer.close()
