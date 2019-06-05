6cbbb134d5eb920ae2392fc08cee76518b3e822c
JasonYao <31233752+JasonYao81000@users.noreply.github.com>
2019-06-05 10:19:56 +0800

    Add spectral normalization for the linear layers.

--- hw4/acgan.py
+++ hw4/acgan.py
@@ -73,11 +73,11 @@ class Discriminator(nn.Module):
         ds_size = IMAGE_SIZE // 2 ** 4
 
         # Output layers
-        self.adv_layer = nn.Sequential(nn.Linear(128 * ds_size ** 2, 1))
-        self.aux_layer_hair = nn.Sequential(nn.Linear(128 * ds_size ** 2, len(datasets.attr_hair)), nn.Softmax(dim=-1))
-        self.aux_layer_eye = nn.Sequential(nn.Linear(128 * ds_size ** 2, len(datasets.attr_eye)), nn.Softmax(dim=-1))
-        self.aux_layer_face = nn.Sequential(nn.Linear(128 * ds_size ** 2, len(datasets.attr_face)), nn.Softmax(dim=-1))
-        self.aux_layer_glasses = nn.Sequential(nn.Linear(128 * ds_size ** 2, len(datasets.attr_glasses)), nn.Softmax(dim=-1))
+        self.adv_layer = nn.Sequential(nn.utils.spectral_norm(nn.Linear(128 * ds_size ** 2, 1)))
+        self.aux_layer_hair = nn.Sequential(nn.utils.spectral_norm(nn.Linear(128 * ds_size ** 2, len(datasets.attr_hair))), nn.Softmax(dim=-1))
+        self.aux_layer_eye = nn.Sequential(nn.utils.spectral_norm(nn.Linear(128 * ds_size ** 2, len(datasets.attr_eye))), nn.Softmax(dim=-1))
+        self.aux_layer_face = nn.Sequential(nn.utils.spectral_norm(nn.Linear(128 * ds_size ** 2, len(datasets.attr_face))), nn.Softmax(dim=-1))
+        self.aux_layer_glasses = nn.Sequential(nn.utils.spectral_norm(nn.Linear(128 * ds_size ** 2, len(datasets.attr_glasses))), nn.Softmax(dim=-1))
 
     def forward(self, img):
         out = self.conv_blocks(img)
--- hw4/resnet_discriminator.py
+++ hw4/resnet_discriminator.py
@@ -140,11 +140,11 @@ class Discriminator(nn.Module):
         flatten_size = 128 * BasicBlock.expansion * ((IMAGE_SIZE // 2 ** 4) ** 2)
 
         # Output layers
-        self.adv_layer = nn.Sequential(nn.Linear(flatten_size, 1))
-        self.aux_layer_hair = nn.Sequential(nn.Linear(flatten_size, len(datasets.attr_hair)), nn.Softmax(dim=-1))
-        self.aux_layer_eye = nn.Sequential(nn.Linear(flatten_size, len(datasets.attr_eye)), nn.Softmax(dim=-1))
-        self.aux_layer_face = nn.Sequential(nn.Linear(flatten_size, len(datasets.attr_face)), nn.Softmax(dim=-1))
-        self.aux_layer_glasses = nn.Sequential(nn.Linear(flatten_size, len(datasets.attr_glasses)), nn.Softmax(dim=-1))
+        self.adv_layer = nn.Sequential(nn.utils.spectral_norm(nn.Linear(flatten_size, 1)))
+        self.aux_layer_hair = nn.Sequential(nn.utils.spectral_norm(nn.Linear(flatten_size, len(datasets.attr_hair))), nn.Softmax(dim=-1))
+        self.aux_layer_eye = nn.Sequential(nn.utils.spectral_norm(nn.Linear(flatten_size, len(datasets.attr_eye))), nn.Softmax(dim=-1))
+        self.aux_layer_face = nn.Sequential(nn.utils.spectral_norm(nn.Linear(flatten_size, len(datasets.attr_face))), nn.Softmax(dim=-1))
+        self.aux_layer_glasses = nn.Sequential(nn.utils.spectral_norm(nn.Linear(flatten_size, len(datasets.attr_glasses))), nn.Softmax(dim=-1))
 
     def forward(self, img):
         out = self.resnet(img)
