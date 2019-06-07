513b8872d3ad11eb491c8d68d4c2593d7c763f29
JasonYao <31233752+JasonYao81000@users.noreply.github.com>
2019-06-07 11:52:50 +0800

    Create training progress GIFs for all architectures.

--- hw4/gif.py
+++ hw4/gif.py
@@ -13,6 +13,30 @@ for i in tqdm(range(100, 500, 50)):
     writer.append_data(im)
 writer.close()
 
+image_dir = './eval_images/resnet_500/'
+writer = imageio.get_writer(image_dir + 'resnet_500.gif', fps=2)
+for i in tqdm(range(0, 100, 5)):
+    fname = image_dir + '%d.png' % (i)
+    im = imageio.imread(fname)
+    writer.append_data(im)
+for i in tqdm(range(100, 500, 50)):
+    fname = image_dir + '%d.png' % (i)
+    im = imageio.imread(fname)
+    writer.append_data(im)
+writer.close()
+
+image_dir = './eval_images/resnet_hinge_500/'
+writer = imageio.get_writer(image_dir + 'resnet_hinge_500.gif', fps=2)
+for i in tqdm(range(0, 100, 5)):
+    fname = image_dir + '%d.png' % (i)
+    im = imageio.imread(fname)
+    writer.append_data(im)
+for i in tqdm(range(100, 500, 50)):
+    fname = image_dir + '%d.png' % (i)
+    im = imageio.imread(fname)
+    writer.append_data(im)
+writer.close()
+
 image_dir = './eval_images/resnet_1000/'
 writer = imageio.get_writer(image_dir + 'resnet_1000.gif', fps=2)
 for i in tqdm(range(0, 100, 5)):
